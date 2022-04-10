f0c96063bd612a8895b444d613a743ca0be27547
Andreasedgarp01 <andreasedgarp@gmail.com>
2022-04-10 13:31:00 +0700

    first commit

---
+++ app/views/about/index.php
@@ -0,0 +1,5 @@
+<div class="container">
+  <h1 class="mt-4">About me</h1>
+  <img src="<?= BASEURL; ?>/img/spongebob.png" width="200" class="rounded-circle shadow">
+  <p>Nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> saya seorang <?= $data['pekerjaan']; ?></p>
+</div>
